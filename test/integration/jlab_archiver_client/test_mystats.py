5dcc392b9820f31a4612961a75a1cbb316bb5b42
Adam Carpenter <adamc@jlab.org>
2025-11-12 18:02:07 -0500

    Add test case for unsupported PV types

--- test/integration/jlab_archiver_client/test_mystats.py
+++ test/integration/jlab_archiver_client/test_mystats.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+import warnings
 from datetime import datetime
 from typing import Tuple, Any, Dict
 
@@ -82,7 +83,48 @@ class TestMyStats(unittest.TestCase):
                     'channel101': {'name': 'channel101', 'datatype': 'DBR_DOUBLE', 'datasize': 1, 'datahost': 'mya',
                                    'ioc': None, 'active': True}}
 
-        return data , metadata
+        return data, metadata
+
+    def test_construct_expected2(self):
+        data = {
+            'channel1': [
+                # 2019-08-12 00:00:00
+                4.292270e+04,  # duration
+                1.809400e+04,  # eventCount
+                3.482493e+06,  # integration
+                9.896990e+01,  # max
+                8.113410e+01,  # mean
+                0.000000e+00,  # min
+                8.609530e+01,  # rms
+                2.880380e+01,  # stdev
+                1.809300e+04,  # updateCount
+                # 2019-08-12 12:00:00
+                4.272572e+04,  # duration
+                1.489600e+04,  # eventCount
+                2.828281e+06,  # integration
+                1.039970e+02,  # max
+                6.619620e+01,  # mean
+                0.000000e+00,  # min
+                7.405190e+01,  # rms
+                3.319260e+01,  # stdev
+                1.489500e+04,  # updateCount
+            ]
+        }
+
+        # Create the MultiIndex
+        timestamps = [
+            pd.Timestamp('2019-08-12 00:00:00'),
+            pd.Timestamp('2019-08-12 12:00:00')
+        ]
+
+        stats = ['duration', 'eventCount', 'integration', 'max', 'mean', 'min', 'rms', 'stdev', 'updateCount']
+
+        # Create tuples for MultiIndex
+        index_tuples = [(ts, stat) for ts in timestamps for stat in stats]
+        index = pd.MultiIndex.from_tuples(index_tuples, names=['timestamp', 'stat'])
+
+        # Create the DataFrame
+        return pd.DataFrame(data, index=index)
 
     def test_get_mystats(self):
         """Test basic usage"""
@@ -99,6 +141,33 @@ class TestMyStats(unittest.TestCase):
 
         res_data = mystats.data
         res_metadata = mystats.metadata
+
+        pd.testing.assert_frame_equal(exp_data, res_data)
+        self.assertDictEqual(exp_metadata, res_metadata)
+
+    def test_get_mystats_2(self):
+        """Test basic usage with an unsupported Enum PV"""
+        query = MyStatsQuery(
+            start=datetime.strptime("2019-08-12 00:00:00", "%Y-%m-%d %H:%M:%S"),
+            end=datetime.strptime("2019-08-13 00:00:00", "%Y-%m-%d %H:%M:%S"),
+            num_bins=2,  # 24 hours of data
+            pvlist=["channel1", "channel2"],
+            deployment="docker"
+        )
+        mystats = MyStats(query)
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            mystats.run()
+            self.assertEqual(len(w), 1)
+            self.assertEqual(str(w[0].message),
+                             "Error querying channel2: This myStats only supports FloatEvents - not 'org.jlab.mya.event.IntEvent'.")
+
+        res_data, res_metadata = mystats.data, mystats.metadata
+
+        exp_data = self.test_construct_expected2()
+        exp_metadata = {
+            'channel1': {'name': 'channel1', 'datatype': 'DBR_DOUBLE', 'datasize': 1, 'datahost': 'mya', 'ioc': None,
+                         'active': True}}
 
         pd.testing.assert_frame_equal(exp_data, res_data)
         self.assertDictEqual(exp_metadata, res_metadata)
