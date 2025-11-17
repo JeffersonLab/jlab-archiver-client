f1b5c5097eb666060bc10290d0303c79b9c17e1f
Adam Carpenter <adamc@jlab.org>
2025-11-17 12:14:35 -0500

    linting

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from jlab_archiver_client.config import config
 
-#
+
 @pytest.fixture(autouse=True, scope="session")
 def _integration_config():
     config.set(myquery_server="localhost:8080", protocol="http")
