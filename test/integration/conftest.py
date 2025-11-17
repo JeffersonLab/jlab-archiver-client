8ef0cffa0ab83280c376565c10909550cb95094e
Adam Carpenter <adamc@jlab.org>
2025-11-17 12:14:07 -0500

    Refactor config into pytest session fixture

---
+++ test/integration/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+from jlab_archiver_client.config import config
+
+#
+@pytest.fixture(autouse=True, scope="session")
+def _integration_config():
+    config.set(myquery_server="localhost:8080", protocol="http")
--- test/integration/jlab_archiver_client/__init__.py
+++
@@ -1,3 +0,0 @@
-from jlab_archiver_client.config import config
-
-config.set(myquery_server = "localhost:8080", protocol = "http")
